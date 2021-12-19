023420f4cbec2fd1dfb6e3e3cce167ffa5e9b8a1
sathish <sathish09117@gmail.com>
2021-12-19 12:00:12 +0530

    Initial

---
+++ app/src/main/java/com/smarttech/cameraintent/CameraIntentActivity.java
@@ -0,0 +1,167 @@
+package com.smarttech.cameraintent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+
+import androidx.core.content.FileProvider;
+import androidx.exifinterface.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+public class CameraIntentActivity extends AppCompatActivity {
+
+    private static final int ACTIVITY_CAMERA_APP = 0;
+    private static final int REQUEST_EXTERNAL_STORAGE_RESULT = 1;
+    private ImageView mPhotoCapImgView;
+
+    private String mImageFileLocation = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPhotoCapImgView = findViewById(R.id.imageView);
+
+    }
+
+    public void takePhoto(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        {
+            callCameraApp();
+        }
+        else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                    Toast.makeText(this, "External storage permission required to save images", Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_EXTERNAL_STORAGE_RESULT);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_EXTERNAL_STORAGE_RESULT){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                callCameraApp();
+            } else {
+                Toast.makeText(this, "External write permission has not been granted, cannot saved images", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+
+    private void callCameraApp(){
+        Intent callCameraApplicationIntent= new Intent();
+        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = null;
+        try{
+            photoFile = createImageFile();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        String authorites = getApplicationContext().getPackageName() + ".fileprovider";
+        Uri imageUri = FileProvider.getUriForFile(this,authorites, photoFile);
+        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+
+        startActivityForResult(callCameraApplicationIntent,ACTIVITY_CAMERA_APP);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == ACTIVITY_CAMERA_APP && resultCode == RESULT_OK){
+            //Toast.makeText(this, "Picture taken successfully", Toast.LENGTH_SHORT).show();
+//            Bundle extras = data.getExtras();
+//            Bitmap photoCApBitmap = (Bitmap) extras.get("data");
+//            mPhotoCapImgView.setImageBitmap(photoCApBitmap);
+//            Bitmap photoCapBitmap = BitmapFactory.decodeFile(mImageFileLocation);
+//            mPhotoCapImgView.setImageBitmap(photoCapBitmap);
+            rotateImage(setReduceImageSize());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    File createImageFile() throws IOException {
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imgeFleName = "IMAGE_" + timeStamp + "_";
+            File storeDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+            File image = File.createTempFile(imgeFleName,".jpg", storeDirectory);
+            mImageFileLocation = image.getAbsolutePath();
+
+            return image;
+
+
+    }
+
+    private Bitmap setReduceImageSize(){
+        int targetImageViewWidth = mPhotoCapImgView.getWidth();
+        int targetImageViewHeight = mPhotoCapImgView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+        int cameraImageWidth = bmOptions.outWidth;
+        int cameraImageHeight = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(cameraImageWidth/targetImageViewWidth, cameraImageHeight/targetImageViewHeight);
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inJustDecodeBounds = false;
+
+//        Bitmap photoReducedSizeBitmap = BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+//        mPhotoCapImgView.setImageBitmap(photoReducedSizeBitmap);
+        return BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+    }
+
+    private void rotateImage(Bitmap bitmap){
+        ExifInterface exifInterface = null;
+        try {
+            exifInterface = new ExifInterface(mImageFileLocation);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,ExifInterface.ORIENTATION_UNDEFINED);
+        Matrix matrix = new Matrix();
+        switch (orientation){
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            default:
+
+        }
+        Bitmap rotateBimap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        mPhotoCapImgView.setImageBitmap(rotateBimap);
+    }
+
+}
